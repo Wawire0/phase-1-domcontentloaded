0567c97497c013b2dc75a178ff14d379b43ed123
Sandra <listerbernice@gmail.com>
2023-09-30 19:45:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const textParagraph = document.getElementById('text');
+  
+    textParagraph.textContent = "This is really cool!";
+  });
+  
